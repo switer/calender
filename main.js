6c7097c37dfbb94316403e9843dcc59beb895585
switer <guankaishe@gmail.com>
2013-07-22 20:38:25 +0800

    update

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 		today = new Date(),
 		data = cal.getDataByDate(today),
 		days2dArray = cal.to2dArray(today),
-		weekLength = 7;
+		weekLength = 7,
+		saveData = window.localStorage.getItem('couter_save_data') || {};
 
 	var main = {
 
@@ -16,14 +17,14 @@
 		renderCal : function (dataArray) {
 			var tableStr = '',
 				weeklyStr = '<tr>',
-				todayStr = data.year + ' / ' + data.month + ' / ' + data.day,
+				todayStr = data.year + '/' + data.month + '/' + data.day,
 				weekly = cal.weekly,
 				curDateStr = '',
 				btnStr = '<div class="btn-group edit-icons" style="visibility:hidden;">' + 
-					'<button class="btn btn-mini"><i class="icon-plus"></i></button>' + 
-					'<button class="btn btn-mini"><i class="icon-minus"></i></button>'
+					'<button class="btn btn-mini edit-plus"><i class="icon-plus"></i></button>' + 
+					'<button class="btn btn-mini edit-minus"><i class="icon-minus"></i></button>'
 			for (var i = 0; i < weekly.length; i ++) {
-				weeklyStr += '<th><div>' + weekly[i] + '</div></th>';
+				weeklyStr += '<th>' + weekly[i] + '</th>';
 			}
 			weeklyStr += '</tr>';
 			tableStr += weeklyStr;
@@ -33,9 +34,10 @@
 					tupleRowStr = '<tr>';
 				for (var j = 0; j < weekLength; j ++) {
 					if (tupleArray[j] !== undefined && tupleArray[j] !== null) {
-						curDateStr = data.year + ' / ' + data.month + ' / ' + tupleArray[j];
+						curDateStr = data.year + '/' + data.month + '/' + tupleArray[j];
 						var tupleClass = (curDateStr === todayStr) ? 'cur' : '';
-						tupleRowStr += '<td class=' + tupleClass + '><center>' + tupleArray[j] + '</center>' + btnStr + '</td>'
+						tupleRowStr += '<td class="' + tupleClass + '" data-time="' + curDateStr + '"><center>' + 
+								tupleArray[j] + '</center><center>' + btnStr + '</center></td>'
 					} else {
 						tupleRowStr += '<td></td>'
 					}
@@ -48,6 +50,7 @@
 			$('#dispDate').html('今天 ' + todayStr + '  ' + data.week)
 		},
 		initEvents : function () {
+			var _this = this;
 			$('#showEditsBtn').on('click', function (e) {
 				console.log('click');
 				var $tar = $('#showEditsBtn');
@@ -59,6 +62,25 @@
 					$tar.data('isShow', 'false')
 				}
 			})
+
+			$('.edit-plus').on('click', function (e) {
+				console.log(e.target);
+				var tarTd = _this._getParent($(e.target));
+				console.log(tarTd[0])
+
+			})
+			$('.edit-minus').on('click', function (e) {
+				console.log(e.target);
+				var tarTd = _this._getParent($(e.target));
+				console.log(tarTd[0])
+			})
+			
+		},
+		_getParent : function ($tar) {
+			var $parent = $tar.parent().parent().parent();
+			console.log($parent.data('time'))
+			if ($parent.data('time')) return $parent;
+			else return $parent.parent();
 		}
 	}
 	
